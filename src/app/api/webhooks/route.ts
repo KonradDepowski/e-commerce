116dc8396dfb83b96b21b4e591d26ac05fef61f6
KonradDepowski <konradskontakt@gmail.com>
2024-03-22 17:05:50 +0100

    change auth

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -61,15 +61,13 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name } = evt.data;
 
-    const newUser = {
+    await createUser({
       clerkId: id,
       email: email_addresses[0].email_address,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
-    };
-
-    await createUser(newUser);
+    });
   }
   // if (eventType === "user.updated") {
   //   const { id, email_addresses, image_url, first_name, last_name } = evt.data;
