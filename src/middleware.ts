5f77c7aeceab4066045dc0d6984f01fe8d3e32b8
KonradDepowski <depowskikonrad@gmail.com>
2024-11-20 10:51:54 +0100

    fix middlwaare error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/login", "/", "/api/webhook(.*)", "/api/webhook/stripe"],
+  publicRoutes: [
+    "/login",
+    "/",
+    "/api/webhook(.*)",
+    "/api/webhook/stripe",
+    "/_next/static/chunks/app/page.js", // Add specific static assets here
+    "/favicon.ico", // Allow public access to favicon
+  ],
   ignoredRoutes: ["/no-auth-in-this-route"],
 });
+
+export const config = {
+  matcher: [
+    "/api/webhooks(.*)",
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
+};
