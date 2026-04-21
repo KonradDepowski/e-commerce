5aa486448e145e4d3026b0eff2278891f5aa6c53
KonradDepowski <depowskikonrad@gmail.com>
2026-04-21 17:16:33 +0200

    remvoe userProfile component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { fetchUserOrder } from "@/lib/actions/order";
-import { useAuth, UserProfile } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import { SignOutButton } from "../buttons/SignOutButton";
-import { dark } from "@clerk/ui/themes";
 import { useTheme } from "next-themes";
-import type { Theme } from "@clerk/types";
 import OrdersList from "../order/OrdersList";
 import Loader from "../Loader/Loader";
 import { orderSchemaType } from "@/lib/types/types";
@@ -14,7 +12,6 @@ const Profile = () => {
   const { userId } = useAuth();
   const [orders, setOrders] = useState<orderSchemaType[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const { theme } = useTheme();
 
   useEffect(() => {
     const fetchUserOrders = async () => {
@@ -29,12 +26,8 @@ const Profile = () => {
     fetchUserOrders();
   }, [userId]);
 
-  const appearanceConfig: Theme | undefined =
-    theme === "dark" ? { baseTheme: dark } : undefined;
-
   return (
     <section className="flex flex-row flex-wrap justify-center py-4 gap-5 w-full  ">
-      <UserProfile appearance={appearanceConfig} />
       <div className="dark:bg-primary bg-white shadow-2xl w-[95%] min-h-[40vh] max-w-[40rem] p-4 xl:p-8 rounded-xl relative pb-12">
         <h2 className="text-2xl font-bold py-3 xl:text-3xl text-[var(--green-main)]">
           My Orders
